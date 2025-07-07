7600b664248ae9b45208cd39730218769f5b560b
ebranfazza@gmail.com <Fasal2531@>
2025-07-07 12:58:30 +0530

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,8 +12,8 @@ const Header = () => {
             </div>
             <div>
                 <ul className="flex gap-2 m-3 items-center">
-                    <li className="px-4 ">Home</li>
-                    <li className="px-4 bg-black text-white rounded "><Link to={'/cart'}>Cart</Link></li>
+                    <li className="px-4 hover:bg-gray-400 rounded transition-colors duration-200 cursor-pointer text-2xl">Home</li>
+                    <li className="px-6 bg-black text-white rounded hover:bg-gray-700 transition-colors duration-200 cursor-pointe text-2xl"><Link to={'/cart'}>Cart</Link></li>
                 </ul>
             </div>
         </div>
